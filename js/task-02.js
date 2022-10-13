d59f322f02528bc2257f1c82dc3441f8186366a9
BartekJedliczka <bartekjedliczka@gmail.com>
2022-10-13 21:33:18 +0200

    Zadanie 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const list = document.querySelector("#ingredients");
+
+const liArray = [];
+
+// list.append(...liArray);
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.className = "item";
+  item.textContent = ingredient;
+  liArray.push(item);
+});
+
+list.append(...liArray);

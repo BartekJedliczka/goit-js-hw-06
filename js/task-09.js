37edd6dffd008e8ad052e13aadb2c5addb1080ef
BartekJedliczka <bartekjedliczka@gmail.com>
2022-10-30 15:31:26 +0100

    zadanie 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const colorValue = document.querySelector("span.color");
+const button = document.querySelector("button.change-color");
+
+button.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+});

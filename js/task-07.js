17f80a945d2047ab62f46a931872cf0192bc275d
BartekJedliczka <bartekjedliczka@gmail.com>
2022-11-03 12:45:36 +0100

    task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputControl = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
-inputControl.addEventListener("change", (event) => {
+inputControl.addEventListener("input", (event) => {
   text.style.fontSize = `${event.target.value}px`;
 });
